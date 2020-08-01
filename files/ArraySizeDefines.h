177093348eebb9a92c507375246c6bd199933fa2
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-01 10:36:31 +0200

    Update ArraySizeDefines.h

--- files/ArraySizeDefines.h
+++ files/ArraySizeDefines.h
@@ -13,7 +13,7 @@
 // The above is reported buggy, so it is recommended adding assert for safety, source: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=94746)
 // #define ARRAY_SIZE(arr)     (sizeof(arr) / sizeof((arr)[0]) + Static_assert_eval0_array(arr))
 
-// ** The size of the elements of the array
+// ** The size of the elements of the array (data-type size)
 #define ARRAY_SSIZE(arr)    ((ptrdiff_t)ARRAY_SIZE(arr))
 
 // ** The size in bytes that the array uses in memory
