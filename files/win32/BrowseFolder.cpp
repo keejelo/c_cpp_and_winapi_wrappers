33ad6cbf65352a78c725f41a7bcd87a4a80bcb2b
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-01 09:53:01 +0200

    Update BrowseFolder.cpp

--- files/win32/BrowseFolder.cpp
+++ files/win32/BrowseFolder.cpp
@@ -14,22 +14,22 @@
 //---------------------------------------------------------------------------------------------
 static BOOL CALLBACK EnumCallback(HWND hWndChild, LPARAM lParam)
 {
-	char szClass[MAX_PATH];
-	HTREEITEM hNode;
-	if (GetClassName(hWndChild, szClass, sizeof(szClass)) && strcmp(szClass, "SysTreeView32") == 0)
-	{
-		// ** Found the tree view window
-		hNode = TreeView_GetSelection(hWndChild);
+    char szClass[MAX_PATH];
+    HTREEITEM hNode;
+    if (GetClassName(hWndChild, szClass, sizeof(szClass)) && strcmp(szClass, "SysTreeView32") == 0)
+    {
+        // ** Found the tree view window
+        hNode = TreeView_GetSelection(hWndChild);
 
-		// ** Ensure its selection is visible
-		TreeView_EnsureVisible(hWndChild, hNode);
+        // ** Ensure its selection is visible
+        TreeView_EnsureVisible(hWndChild, hNode);
 
-		// ** Done; stop enumerating
-		return false;	
-	}
+        // ** Done; stop enumerating
+        return false;	
+    }
 
-	// ** Continue enumerating
-	return true;		
+    // ** Continue enumerating
+    return true;
 };
 //---------------------------------------------------------------------------------------------
 // ** END: EnumCallback - Callback function for EnumWindows 
@@ -41,22 +41,20 @@ static BOOL CALLBACK EnumCallback(HWND hWndChild, LPARAM lParam)
 //---------------------------------------------------------------------------------------------
 static int CALLBACK BrowseCallbackProc(HWND hWnd, UINT uMsg, LPARAM lParam, LPARAM lpData)
 {
-	switch (uMsg)
-	{
-		case BFFM_INITIALIZED:
-			// ** Expand the tree view
-			SendMessage(hWnd, BFFM_SETEXPANDED, TRUE, lpData);
+    switch (uMsg)
+    {
+        case BFFM_INITIALIZED:
+            // ** Expand the tree view
+            SendMessage(hWnd, BFFM_SETEXPANDED, TRUE, lpData);
+            // ** Select the item
+            SendMessage(hWnd, BFFM_SETSELECTION, TRUE, lpData);
+            break;
 
-			// ** Select the item
-			SendMessage(hWnd, BFFM_SETSELECTION, TRUE, lpData);
-
-			break;
-
-		case BFFM_SELCHANGED:
-			EnumChildWindows(hWnd, EnumCallback, 0);
-			break;
-	}
-	return 0;
+        case BFFM_SELCHANGED:
+            EnumChildWindows(hWnd, EnumCallback, 0);
+            break;
+    }
+    return 0;
 };
 //---------------------------------------------------------------------------------------------
 // ** END: Callback to show initial dir ...
@@ -68,39 +66,39 @@ static int CALLBACK BrowseCallbackProc(HWND hWnd, UINT uMsg, LPARAM lParam, LPAR
 //---------------------------------------------------------------------------------------------
 std::string BrowseFolder(HWND hWnd, std::string strInitialDir)
 {
-	// ** Create a buffer to store the path
-	char path[MAX_PATH];
+    // ** Create a buffer to store the path
+    char path[MAX_PATH];
 
-	const char *path_param = strInitialDir.c_str();
+    const char *path_param = strInitialDir.c_str();
 
-	// ** Create BROWSEINFO structure
-	BROWSEINFO bi = { 0 };
-	bi.hwndOwner = hWnd;
-	//bi.lpszTitle = "Some text here";
-	bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;
-	bi.lParam = (LPARAM)path_param;
-	bi.lpfn = BrowseCallbackProc;
-	
-	// ** Show the dialog and get the itemIDList for the selected folder.
-	LPITEMIDLIST pIDL = SHBrowseForFolder(&bi);
+    // ** Create BROWSEINFO structure
+    BROWSEINFO bi = { 0 };
+    bi.hwndOwner = hWnd;
+    //bi.lpszTitle = "Some text here";
+    bi.ulFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE;
+    bi.lParam = (LPARAM)path_param;
+    bi.lpfn = BrowseCallbackProc;
 
-	if (pIDL != NULL)
-	{
-		SHGetPathFromIDList(pIDL, path);
+    // ** Show the dialog and get the itemIDList for the selected folder.
+    LPITEMIDLIST pIDL = SHBrowseForFolder(&bi);
 
-		// ** Free memory used
-		IMalloc *imalloc = 0;
-		if (SUCCEEDED(SHGetMalloc(&imalloc)))
-		{
-			imalloc->Free(pIDL);
-			imalloc->Release();
-		}
-		
-		// ** Return the string
-		return path;
-	}
-	
-	return "";
+    if (pIDL != NULL)
+    {
+        SHGetPathFromIDList(pIDL, path);
+
+        // ** Free memory used
+        IMalloc *imalloc = 0;
+        if (SUCCEEDED(SHGetMalloc(&imalloc)))
+        {
+            imalloc->Free(pIDL);
+            imalloc->Release();
+        }
+
+        // ** Return the string
+        return path;
+    }
+
+    return "";
 };
 //---------------------------------------------------------------------------------------------
 // ** END: Browse for folder and return its path as string
