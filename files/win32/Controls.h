4b387382afe1ac4d76c3907a7d60357f50a928ec
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-02 09:16:44 +0200

    Update Controls.h

--- files/win32/Controls.h
+++ files/win32/Controls.h
@@ -20,7 +20,7 @@ HWND CreateStaticTextCtrl(HWND hWnd, const char szText[], int x, int y, int w = 
 HWND CreateStaticLineHoriz(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL);
 HWND CreateStaticLineVert(HWND hWnd, const char szText[], int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL);
 HWND CreateButtonCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 14, DWORD dwStyle = NULL, void *fnCallback = NULL);
-HWND CreateEditCtrl(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 20, DWORD dwStyle = NULL, int iMaxLength = NULL);
+HWND CreateEditCtrl(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 20, DWORD dwStyle = NULL, int iMaxLength = NULL, bool bNumbersOnly = false);
 HWND CreateComboBoxDropDownCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 12);
 HWND CreateComboBoxSimpleCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 50);
 HWND CreateDropDownListCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 12);
