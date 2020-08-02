38e4e78b1a3c3e881b1a3967ddcd116140340b8a
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-02 18:12:43 +0200

    Update WinMain_template.cpp

--- files/win32/WinMain_template.cpp
+++ files/win32/WinMain_template.cpp
@@ -44,7 +44,7 @@ void CenterWindow(HWND hWnd);
 //---------------------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {    
-    static char szAppName[] = "WindowClassName";               // Define window class name
+    static char szAppName[] = "WindowClassName";               // Window class name
     HWND hWnd;                                                 // Window handle
     MSG msg;                                                   // Window message structure
     WNDCLASSEX wc;                                             // Structure to hold window's attributes
