102fe9222ee30c7bfddd2550298273931bfc6d76
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-02 08:39:31 +0200

    Update Controls.h

--- files/win32/Controls.h
+++ files/win32/Controls.h
@@ -17,6 +17,7 @@
 //---------------------------------------------------------------------------------------------
 HWND CreateStaticTextCtrl(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 30, DWORD dwStyle = NULL);
 HWND CreateStaticLineHoriz(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL);
+HWND CreateStaticLineVert(HWND hWnd, const char szText[], int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL);
 HWND CreateButtonCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 14, DWORD dwStyle = NULL, void *fnCallback = NULL);
 HWND CreateEditCtrl(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 20, DWORD dwStyle = NULL, int iMaxLength = NULL);
 
