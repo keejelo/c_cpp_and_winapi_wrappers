2a11222f17d132c7b37a7232025b48efce828572
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-02 08:56:03 +0200

    Update Controls.h

--- files/win32/Controls.h
+++ files/win32/Controls.h
@@ -15,11 +15,19 @@
 //---------------------------------------------------------------------------------------------
 // ** Function prototypes
 //---------------------------------------------------------------------------------------------
+// ** Controls
 HWND CreateStaticTextCtrl(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 30, DWORD dwStyle = NULL);
 HWND CreateStaticLineHoriz(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL);
 HWND CreateStaticLineVert(HWND hWnd, const char szText[], int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL);
 HWND CreateButtonCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 14, DWORD dwStyle = NULL, void *fnCallback = NULL);
 HWND CreateEditCtrl(HWND hWnd, const char szText[], int x, int y, int w = 100, int h = 20, DWORD dwStyle = NULL, int iMaxLength = NULL);
+HWND CreateComboBoxDropDownCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 12);
+HWND CreateComboBoxSimpleCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 50);
+HWND CreateDropDownListCtrl(HWND hWnd, const char szText[], int x, int y, int w = 50, int h = 12);
+
+// ** Control helpers
+UINT AddStringCombo(const HWND hComboCtrl, const char szText[]);
+
 
 
 
