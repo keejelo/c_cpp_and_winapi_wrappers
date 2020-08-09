830c51f80ad4ed5925858ae4ad4132c3be93f6ad
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-09 16:02:19 +0200

    Update Controls.cpp

--- files/win32/Controls.cpp
+++ files/win32/Controls.cpp
@@ -74,7 +74,7 @@ HWND CreateStaticLineHoriz(HWND hParentWnd, int x, int y, int w, int h, DWORD dw
 //---------------------------------------------------------------------------------------------
 // ** Create static line vertical
 //---------------------------------------------------------------------------------------------
-HWND CreateStaticLineVert(HWND hWndParent, int x, int y, int h, int w, DWORD dwStyle)
+HWND CreateStaticLineVert(HWND hParentWnd, int x, int y, int h, int w, DWORD dwStyle)
 {
     LONG units = GetDialogBaseUnits();
     w = MulDiv(LOWORD(units), w, 4);
@@ -91,9 +91,9 @@ HWND CreateStaticLineVert(HWND hWndParent, int x, int y, int h, int w, DWORD dwS
         y,              // Vertical position
         w,              // Width
         h,              // Height
-        hWndParent,     // Handle to parent window
+        hParentWnd,     // Handle to parent window
         NULL,           // Handle to a menu
-        (HINSTANCE)GetWindowLongPtr(hWndParent, GWLP_HINSTANCE),    // Handle to the instance of the module
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
         NULL);          // No pointer
 };
 //---------------------------------------------------------------------------------------------
@@ -196,14 +196,14 @@ HWND CreateComboBoxDropDownCtrl(HWND hParentWnd, int x, int y, int w, int h)
         dwExStyle,      // Extended window style
         "combobox",     // Predefined system class
         "",             // Window name, or control captiontext
-        dwStyle,        // Styles
-        x,              // X position 
-        y,              // Y position 
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
         w,              // Width
         h,              // Height
-        hParentWnd,     // Parent window
-        NULL,           // No menu
-        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
         NULL);          // No pointer
 };
 //---------------------------------------------------------------------------------------------
@@ -228,14 +228,14 @@ HWND CreateComboBoxSimpleCtrl(HWND hParentWnd, int x, int y, int w, int h)
         dwExStyle,      // Extended window style
         "combobox",     // Predefined system class
         "",             // Window name, or control captiontext
-        dwStyle,        // Styles
-        x,              // X position 
-        y,              // Y position 
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
         w,              // Width
         h,              // Height
-        hParentWnd,     // Parent window
-        NULL,           // No menu
-        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
         NULL);          // No pointer
 };
 //---------------------------------------------------------------------------------------------
@@ -260,14 +260,14 @@ HWND CreateDropDownListCtrl(HWND hParentWnd, int x, int y, int w, int h)
         dwExStyle,      // Extended window style
         "combobox",     // Predefined system class
         "",             // Window name, or control captiontext
-        dwStyle,        // Styles
-        x,              // X position 
-        y,              // Y position 
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
         w,              // Width
         h,              // Height
-        hParentWnd,     // Parent window
-        NULL,           // No menu
-        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
         NULL);          // No pointer
 };
 //---------------------------------------------------------------------------------------------
@@ -286,5 +286,143 @@ UINT AddStringCombo(const HWND hCombo, const char szText[])
 // ** END: Add string to combobox
 //---------------------------------------------------------------------------------------------
 
+
+//---------------------------------------------------------------------------------------------
+// ** Create listbox control
+//---------------------------------------------------------------------------------------------
+HWND CreateListBoxCtrl(HWND hParentWnd, int x, int y, int w, int h, DWORD dwExStyle)
+{
+    LONG units = GetDialogBaseUnits();
+    w = MulDiv(LOWORD(units), w, 4);   // 50
+    h = MulDiv(HIWORD(units), h, 10);  // 12
+
+    // ** Add styles
+    DWORD dwStyle = WS_VISIBLE | WS_CHILD | LBS_STANDARD;
+
+    return CreateWindowEx(
+        dwExStyle,      // Extended window style
+        "listbox",      // Predefined class in "windows.h"
+        "",             // Window name, or control captiontext
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
+        w,              // Width
+        h,              // Height
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
+        NULL);          // No pointer
+};
+//---------------------------------------------------------------------------------------------
+// ** END: Create listbox control
+//---------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------
+// ** Add string to listbox
+//---------------------------------------------------------------------------------------------
+UINT AddStringListBox(const HWND hListBox, const char szText[])
+{
+    return static_cast<UINT>(SendMessage(hListBox, LB_ADDSTRING, 0, reinterpret_cast<LPARAM>(szText)));
+};
+//---------------------------------------------------------------------------------------------
+// ** END: Add items to listbox
+//---------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------
+// ** Create checkbox control
+//---------------------------------------------------------------------------------------------
+HWND CreateCheckBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w, int h, DWORD dwStyle)
+{
+    LONG units = GetDialogBaseUnits();
+    w = MulDiv(LOWORD(units), w, 4);
+    h = MulDiv(HIWORD(units), h, 8);
+
+    dwStyle |= WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX;
+
+    return CreateWindowEx(
+        NULL,           // Extended window style
+        "button",       // Predefined system class
+        szText,         // Window name, or control captiontext
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
+        w,              // Width
+        h,              // Height
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
+        NULL);          // No pointer
+};
+//---------------------------------------------------------------------------------------------
+// ** END: Create checkbox control
+//---------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------
+// ** Create radiobutton control
+//---------------------------------------------------------------------------------------------
+HWND CreateRadioButtonCtrl(HWND hParentWnd, const char szText[], int x, int y, int w, int h, DWORD dwStyle)
+{
+    LONG units = GetDialogBaseUnits();
+    w = MulDiv(LOWORD(units), w, 4);
+    h = MulDiv(HIWORD(units), h, 8);
+
+    dwStyle |= WS_TABSTOP | WS_VISIBLE | WS_CHILD /*| WS_GROUP */ | BS_AUTORADIOBUTTON;
+
+    return CreateWindowEx(
+        NULL,           // Extended window style
+        "button",       // Predefined system class
+        szText,         // Window name, or control captiontext
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
+        w,              // Width
+        h,              // Height
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
+        NULL);          // No pointer
+
+    /*
+    Group the radio boxes:
+    Put the WS_GROUP in the first radio button's style.
+    It will group the current radio button until it see the second WS_GROUP
+    */
+};
+//---------------------------------------------------------------------------------------------
+// ** END: Create radiobutton control
+//---------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------
+// ** Create groupbox control
+//---------------------------------------------------------------------------------------------
+HWND CreateGroupBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w, int h)
+{
+    LONG units = GetDialogBaseUnits();
+    w = MulDiv(LOWORD(units), w, 4);
+    h = MulDiv(HIWORD(units), h, 8);
+
+    DWORD dwStyle = WS_VISIBLE | WS_CHILD | BS_GROUPBOX;
+
+    return CreateWindowEx(
+        NULL,           // Extended window style
+        "button",       // Predefined system class
+        szText,         // Window name, or control captiontext
+        dwStyle,        // Window style
+        x,              // Horizontal position
+        y,              // Vertical position
+        w,              // Width
+        h,              // Height
+        hParentWnd,     // Handle to parent window
+        NULL,           // Handle to a menu
+        (HINSTANCE)GetWindowLongPtr(hParentWnd, GWLP_HINSTANCE),    // Handle to the instance of the module
+        NULL);          // No pointer
+};
+//---------------------------------------------------------------------------------------------
+// ** END: Create groupbox control
+//---------------------------------------------------------------------------------------------
 
 
