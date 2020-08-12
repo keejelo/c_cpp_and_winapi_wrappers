2e7959139770a34a6a8330342f820925e176885a
keejelo <34620895+keejelo@users.noreply.github.com>
2020-08-12 08:35:08 +0200

    Update Controls.h

--- files/win32/controls/Controls.h
+++ files/win32/controls/Controls.h
@@ -15,7 +15,6 @@
 //---------------------------------------------------------------------------------------------
 // ** FUNCTION PROTOTYPES
 //---------------------------------------------------------------------------------------------
-// ** Controls
 HWND CreateStaticTextCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 30, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateStaticLineHoriz(HWND hParentWnd, int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateStaticLineVert(HWND hParentWnd, int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
@@ -29,7 +28,7 @@ HWND CreateCheckBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int 
 HWND CreateRadioButtonCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateGroupBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 50, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 
-// ** Control helpers
+// ** Helpers
 UINT AddStringCombo(const HWND hComboCtrl, const char szText[]);
 UINT AddStringListBox(const HWND hListBoxCtrl, const char szText[]);
 
