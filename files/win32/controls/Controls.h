bb8740173289ea91f7b696acabdc4a660d30737b
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-24 16:38:38 +0100

    Update Controls.h

--- files/win32/controls/Controls.h
+++ files/win32/controls/Controls.h
@@ -7,28 +7,29 @@
 //---------------------------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------------------------
-// ** INCLUDE FILES
+// ** Include files
 //---------------------------------------------------------------------------------------------
 #include <windows.h>
 
 
 //---------------------------------------------------------------------------------------------
-// ** FUNCTION PROTOTYPES
+// ** Function prototypes
 //---------------------------------------------------------------------------------------------
-HWND CreateStaticTextCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 30, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateStaticLineHoriz(HWND hParentWnd, int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateStaticLineVert(HWND hParentWnd, int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateButtonCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 14, DWORD dwStyle = NULL, DWORD dwExStyle = NULL, void *fnCallback = NULL);
-HWND CreateEditCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 14, int iMaxLength = 0, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateComboBoxDropDownCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateComboBoxSimpleCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 50, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateDropDownListCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateListBoxCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 50, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateCheckBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateRadioButtonCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateGroupBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 50, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+// ** Controls
+HWND CreateStaticTextCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 30, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateStaticLineHoriz(HWND hParentWnd, int x, int y, int w = 100, int h = 5, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateStaticLineVert(HWND hParentWnd, int x, int y, int h = 100, int w = 5, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateButtonCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 14, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL, void *fnCallback = NULL);
+HWND CreateEditCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 14, int ctrlID = NULL, int iMaxLength = 0, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateComboBoxDropDownCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 12, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateComboBoxSimpleCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 50, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateDropDownListCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 12, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateListBoxCtrl(HWND hParentWnd, int x, int y, int w = 50, int h = 50, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateCheckBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 12, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateRadioButtonCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 50, int h = 12, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateGroupBoxCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 50, int ctrlID = NULL, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 
-// ** Helpers
+// ** Control helpers
 UINT AddStringCombo(const HWND hComboCtrl, const char szText[]);
 UINT AddStringListBox(const HWND hListBoxCtrl, const char szText[]);
 
