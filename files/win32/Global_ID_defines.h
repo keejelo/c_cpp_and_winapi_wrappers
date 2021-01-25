df9af8bb37c1da28bfb47a9b3f2c2cae2fd83de0
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-25 11:49:54 +0100

    Update Global_ID_defines.h

--- files/win32/Global_ID_defines.h
+++ files/win32/Global_ID_defines.h
@@ -10,7 +10,7 @@
 // around in different files, and in worst case some ID's are duplicated and result in strange
 // and unknown errors. Keeping them together is easier and you have total control.
 //
-// The names and values are abitrary (does not matter), but each name and value must be unique.
+// The names and values are arbitrary(does not matter), but each name and value must be unique.
 // Valid range for ID values is 1 through 65565.
 //
 // This file must be included everywhere you use the ID's.
