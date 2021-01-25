f8a5340c5bc86b4e9b4bd2cccb49180c21287fa3
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-25 10:53:20 +0100

    Update Global_control_ID_defines.h

--- files/win32/Global_control_ID_defines.h
+++ files/win32/Global_control_ID_defines.h
@@ -2,15 +2,15 @@
 //---------------------------------------------------------------------------------------------
 // ** Global_control_ID_defines.h
 //---------------------------------------------------------------------------------------------
+#ifndef GLOBAL_CONTROL_ID_DEFINES_H
+#define GLOBAL_CONTROL_ID_DEFINES_H
+//---------------------------------------------------------------------------------------------
 // A file to define all your global ID's, similar to what MFC does for you, but here we have to
 // manage and control it all ourselves. This file must be included everywhere you use the ID's.
 //
 // The names and values are abitrary (does not matter), but each name and value must be unique.
 // Valid range for ID values is 1 through 65565.
 //
-//---------------------------------------------------------------------------------------------
-#ifndef GLOBAL_CONTROL_ID_DEFINES_H
-#define GLOBAL_CONTROL_ID_DEFINES_H
 //---------------------------------------------------------------------------------------------
 
 
