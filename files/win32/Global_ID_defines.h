957adfdd105350dda79b7a1576b0637ccf4984e8
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-25 11:07:28 +0100

    Update Global_ID_defines.h

--- files/win32/Global_ID_defines.h
+++ files/win32/Global_ID_defines.h
@@ -6,10 +6,14 @@
 #define GLOBAL_ID_DEFINES_H
 //---------------------------------------------------------------------------------------------
 // A file to define all your global ID's, similar to what MFC does for you, but here we have to
-// manage and control it all ourselves. This file must be included everywhere you use the ID's.
+// manage and control it all ourselves. It's easier to have them all in one file than scattered
+// around in different files, and in worst case some ID's are duplicated and result in strange
+// and unknown errors. Keeping them together is easier and you have total control.
 //
 // The names and values are abitrary (does not matter), but each name and value must be unique.
 // Valid range for ID values is 1 through 65565.
+//
+// This file must be included everywhere you use the ID's.
 //---------------------------------------------------------------------------------------------
 
 
