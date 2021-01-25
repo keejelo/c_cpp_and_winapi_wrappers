2e1d1f929838fd127f13010390576aace0aa0511
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-25 10:52:10 +0100

    Add files via upload

---
+++ files/win32/Global_control_ID_defines.h
@@ -0,0 +1,43 @@
+#pragma once
+//---------------------------------------------------------------------------------------------
+// ** Global_control_ID_defines.h
+//---------------------------------------------------------------------------------------------
+// A file to define all your global ID's, similar to what MFC does for you, but here we have to
+// manage and control it all ourselves. This file must be included everywhere you use the ID's.
+//
+// The names and values are abitrary (does not matter), but each name and value must be unique.
+// Valid range for ID values is 1 through 65565.
+//
+//---------------------------------------------------------------------------------------------
+#ifndef GLOBAL_CONTROL_ID_DEFINES_H
+#define GLOBAL_CONTROL_ID_DEFINES_H
+//---------------------------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------------------------
+// ** Defines
+//---------------------------------------------------------------------------------------------
+#define IDC_EDIT1        100
+#define IDC_BUTTON1      101
+#define IDC_BUTTON2      102
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------
+#endif // GLOBAL_CONTROL_ID_DEFINES_H
+//---------------------------------------------------------------------------------------------
