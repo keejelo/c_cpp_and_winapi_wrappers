636195262f1b50d46655e3a8fb129918232f77ce
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-26 11:12:51 +0100

    Update Global_ID_defines.h

--- files/win32/Global_ID_defines.h
+++ files/win32/Global_ID_defines.h
@@ -1,14 +1,17 @@
 #pragma once
 //---------------------------------------------------------------------------------------------
-// ** Global_ID_defines.h
+// ** Global_ID_defines.h  - (similar to "resource.h" in MS Visual Studio IDE)
 //---------------------------------------------------------------------------------------------
 #ifndef GLOBAL_ID_DEFINES_H
 #define GLOBAL_ID_DEFINES_H
 //---------------------------------------------------------------------------------------------
-// A file to define all your global ID's, similar to what MFC does for you, but here we have to
-// manage and control it all ourselves. It's easier to have them all in one file than scattered
-// around in different files, and in worst case some ID's are duplicated and result in strange
-// and unknown errors. Keeping them together in one file is easier and you have total control.
+// A file to define all your global ID's, similar to what MFC does for you with "resource.h", 
+// so if you're adding alot of resources to your project you're better off using "resource.h"
+// If not then here we go:
+// Here we have to manage and control it all ourselves. It's easier to have it all in one
+// file than scattered around in different files, and in worst case some ID's are duplicated
+// and result in strange and unknown errors. Keeping them together in one file is easier and
+// you have total control.
 //
 // The names and values are arbitrary, does not matter what you name them, but each name and
 // value must be unique. Valid range for ID values is 1 through 65565.
