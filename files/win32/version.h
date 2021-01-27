d28882c8214ef6c43ba0e324703138fd6e70ef09
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-27 15:55:00 +0100

    Update version.h

--- files/win32/version.h
+++ files/win32/version.h
@@ -5,6 +5,14 @@
 // https://www.zachburlingame.com/2011/02/versioning-a-native-cc-binary-with-visual-studio/
 //---------------------------------------------------------------------------------------------
 
+#define APP_NAME      "My_app_name"          // .exe will be added automatically
+#define APP_DESCRIPT  "My description"
+#define APP_COPYRIGHT "Copyright (C) 2021"
+
+//---------------------------------------------------------------------------------------------
+#include <windows.h>
+//---------------------------------------------------------------------------------------------
+
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
  
@@ -13,19 +21,19 @@
 #define VERSION_REVISION            0
 #define VERSION_BUILD               0
  
-#define VER_FILE_DESCRIPTION_STR    "Description"
+#define VER_FILE_DESCRIPTION_STR    APP_DESCRIPT
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
                                     "." STRINGIZE(VERSION_MINOR)    \
                                     "." STRINGIZE(VERSION_REVISION) \
                                     "." STRINGIZE(VERSION_BUILD)    \
  
-#define VER_PRODUCTNAME_STR         "my_product_name"
+#define VER_PRODUCTNAME_STR         APP_NAME
 #define VER_PRODUCT_VERSION         VER_FILE_VERSION
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
 #define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
 #define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
-#define VER_COPYRIGHT_STR           "Copyright (C) 2021"
+#define VER_COPYRIGHT_STR           APP_COPYRIGHT
  
 #ifdef _DEBUG
   #define VER_VER_DEBUG             VS_FF_DEBUG
