4beb5681f352456eed846f59a1ee6f1f64ff1d93
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-27 16:58:50 +0100

    Update version.h

--- files/win32/version.h
+++ files/win32/version.h
@@ -20,10 +20,10 @@
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
  
-#define VERSION_MAJOR               1
-#define VERSION_MINOR               0
-#define VERSION_REVISION            0
-#define VERSION_BUILD               0
+#define VERSION_MAJOR               1        // major version
+#define VERSION_MINOR               0        // minor version
+#define VERSION_REVISION            0        // revision
+#define VERSION_BUILD               0        // build
  
 #define VER_FILE_DESCRIPTION_STR    APP_DESCRIPTN
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
