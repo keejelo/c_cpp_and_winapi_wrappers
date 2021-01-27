4475b6177c284dd4a447de9c1f11c31ddac76d29
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-27 16:32:43 +0100

    Update version.h

--- files/win32/version.h
+++ files/win32/version.h
@@ -1,15 +1,14 @@
 //---------------------------------------------------------------------------------------------
 // ** version.h 
 //---------------------------------------------------------------------------------------------
-// Add this file and "version.rc" to any project to have versioning, just compile, that's it!
-//---------------------------------------------------------------------------------------------
-// Inspiration:
+// Source:
 // https://www.zachburlingame.com/2011/02/versioning-a-native-cc-binary-with-visual-studio/
 //---------------------------------------------------------------------------------------------
 
-#define APP_NAME      "My_app_name"          // name of executable (.exe will be added auto.)
+#define APP_NAME      "My_app_name"          // .exe will be added automatically
 #define APP_DESCRIPT  "My description"
 #define APP_COPYRIGHT "Copyright (C) 2021"
+#define APP_DEVELOPER "Developer name"
 
 //---------------------------------------------------------------------------------------------
 #include <windows.h>
@@ -29,13 +28,14 @@
                                     "." STRINGIZE(VERSION_MINOR)    \
                                     "." STRINGIZE(VERSION_REVISION) \
                                     "." STRINGIZE(VERSION_BUILD)    \
- 
+
 #define VER_PRODUCTNAME_STR         APP_NAME
 #define VER_PRODUCT_VERSION         VER_FILE_VERSION
 #define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
 #define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".exe"
 #define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
 #define VER_COPYRIGHT_STR           APP_COPYRIGHT
+#define VER_COMPANY_NAME_STR        APP_DEVELOPER
  
 #ifdef _DEBUG
   #define VER_VER_DEBUG             VS_FF_DEBUG
@@ -46,5 +46,4 @@
 #define VER_FILEOS                  VOS_NT_WINDOWS32
 #define VER_FILEFLAGS               VER_VER_DEBUG
 #define VER_FILETYPE                VFT_APP
-
 
