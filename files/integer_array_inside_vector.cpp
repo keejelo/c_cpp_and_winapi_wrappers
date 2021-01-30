ec084d1903c14c33817cee8b0f17c3e80b8eb3a8
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-30 13:45:37 +0100

    Add files via upload

---
+++ files/integer_array_inside_vector.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <vector>
+
+
+int main()
+{    
+    // ** Create struct of an integer array that can hold 3 values
+    struct Int3
+    {
+        int a[3] = { 0 };
+    };
+
+    // ** Create a vector that holds the array struct
+    std::vector<Int3> v;
+    
+    // ** Push back values in vector
+    v.push_back({ 8, 5, 6 });
+
+    // ..or set them one by one
+    // v[0].a[0] = 9;
+    // v[0].a[1] = 4;
+    // v[0].a[2] = 7;
+
+    // ** Output values
+    printf("%d\n", v[0].a[0]);
+    printf("%d\n", v[0].a[1]);
+    printf("%d\n", v[0].a[2]);
+
+    // ** pause before exit
+    getchar();
+    return 0;
+};
