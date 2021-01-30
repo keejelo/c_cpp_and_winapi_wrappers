a32358b52e16065abc3a5b4753ff750dedd0fac6
keejelo <34620895+keejelo@users.noreply.github.com>
2021-01-30 14:08:40 +0100

    Update split_char_array.c

--- files/split_char_array.c
+++ files/split_char_array.c
@@ -13,7 +13,7 @@ int main()
     }
     
     printf("\nInteger hex:\n");
-    for (int i = 0; i < sizeof(str)-1; i++)
+    for (int i = 0; i < sizeof(str) - 1; i++)
     {
         printf("0x%X\n", str[i]);
     }
