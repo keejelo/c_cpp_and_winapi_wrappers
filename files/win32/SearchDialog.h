ea6a52d101485f982754ad7da4aedc6f33df5aa9
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-02 17:13:44 +0100

    Update SearchDialog.h

--- files/win32/SearchDialog.h
+++ files/win32/SearchDialog.h
@@ -21,19 +21,25 @@ extern int SearchList(char *str);
 //---------------------------------------------------------------------------------------------
 // ** Function prototypes
 //---------------------------------------------------------------------------------------------
+// ** Dialog message handler
 LRESULT CALLBACK DialogProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-HWND CreateDialogBox(HWND hWnd, HINSTANCE hInstance, const char *sTitle, int iWidth, int iHeight);
-void RegisterDialogClass(HINSTANCE hInstance);
-
-void ValidateSearch(HWND hWnd);
-void TabFocus(bool bUpDown);
-BOOL CALLBACK EnableTabKey(HWND hWnd, LPARAM lParam);
-BOOL CALLBACK SetCtrlFont(HWND hWnd, LPARAM lParam);
 
 // ** Control message handlers
 LRESULT CALLBACK EditProc(HWND hCtrlWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK OkBtnProc(HWND hCtrlWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK CancelBtnProc(HWND hCtrlWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+void RegisterDialogClass(HINSTANCE hInstance);
+HWND CreateDialogBox(HWND hWnd, HINSTANCE hInstance, const char *sTitle, int iWidth, int iHeight);
+
+BOOL CALLBACK SetCtrlFont(HWND hWnd, LPARAM lParam);
+BOOL CALLBACK EnableTabKey(HWND hWnd, LPARAM lParam);
+
+void TabFocus(bool bUpDown);
+void ValidateSearch(HWND hWnd);
+
+
+
 
 
 //---------------------------------------------------------------------------------------------
