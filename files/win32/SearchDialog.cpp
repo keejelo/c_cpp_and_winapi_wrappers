f2508101318b9948a8c5276e58a9eae16f734e56
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-02 17:35:24 +0100

    Update SearchDialog.cpp

--- files/win32/SearchDialog.cpp
+++ files/win32/SearchDialog.cpp
@@ -295,7 +295,7 @@ BOOL CALLBACK EnableTabKey(HWND hWnd, LPARAM lParam)
 // ** Custom TAB key function to move focus between controls
 //---------------------------------------------------------------------------------------------
 // Since this "dialog" is created with CreateWindowEx we have to handle messages ourselves.
-// I could not get "IsDialogMessage" thing working so I created my own TABkey handler.
+// I could not get "IsDialogMessage" thing working so I created my own TAB key handler.
 //---------------------------------------------------------------------------------------------
 void TabFocus(bool bUpDown)
 {
