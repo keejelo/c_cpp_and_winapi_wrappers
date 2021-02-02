b72402bfd6c878a2c88b53611421a68cd1cd0d41
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-02 16:59:19 +0100

    Update SearchDialog.h

--- files/win32/SearchDialog.h
+++ files/win32/SearchDialog.h
@@ -25,6 +25,8 @@ LRESULT CALLBACK DialogProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 HWND CreateDialogBox(HWND hWnd, HINSTANCE hInstance, const char *sTitle, int iWidth, int iHeight);
 void RegisterDialogClass(HINSTANCE hInstance);
 
+void ValidateSearch(HWND hWnd);
+void TabFocus(bool bUpDown);
 BOOL CALLBACK EnableTabKey(HWND hWnd, LPARAM lParam);
 BOOL CALLBACK SetCtrlFont(HWND hWnd, LPARAM lParam);
 
@@ -32,10 +34,6 @@ BOOL CALLBACK SetCtrlFont(HWND hWnd, LPARAM lParam);
 LRESULT CALLBACK EditProc(HWND hEditWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK OkBtnProc(HWND hOkBtnWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK CancelBtnProc(HWND hCancelBtnWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-void ValidateSearch(HWND hWnd);
-void TabFocus(bool bUpDown);
-
 
 
 //---------------------------------------------------------------------------------------------
