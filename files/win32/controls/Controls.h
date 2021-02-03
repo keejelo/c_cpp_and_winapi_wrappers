4942d278ec15a6cbcd705c88356f5222ed746acc
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-03 11:58:30 +0100

    Update Controls.h

--- files/win32/controls/Controls.h
+++ files/win32/controls/Controls.h
@@ -21,7 +21,7 @@ HWND CreateStaticTextCtrl(HWND hParentWnd, long long int ctrlID, const char szTe
 HWND CreateStaticLineHoriz(HWND hParentWnd, long long int ctrlID, int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateStaticLineVert(HWND hParentWnd, long long int ctrlID, int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateButtonCtrl(HWND hParentWnd, long long int ctrlID, const char szText[], int x, int y, int w = 40, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL, void *fnCallback = NULL);
-HWND CreateEditCtrl(HWND hParentWnd, long long int ctrlID, const char szText[], int x, int y, int w = 50, int h = 14, int iMaxLength = 0, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateEditCtrl(HWND hParentWnd, long long int ctrlID, const char szText[], int x, int y, int w = 50, int h = 14, DWORD dwStyle = NULL, DWORD dwExStyle = NULL, int iMaxLength = 0);
 HWND CreateComboBoxDropDownCtrl(HWND hParentWnd, long long int ctrlID, int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateComboBoxSimpleCtrl(HWND hParentWnd, long long int ctrlID, int x, int y, int w = 50, int h = 50, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateDropDownListCtrl(HWND hParentWnd, long long int ctrlID, int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
