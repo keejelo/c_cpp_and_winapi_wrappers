2f853a4808f85235927a4983efb5392c577a6520
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-04 11:23:30 +0100

    Update Dialog.h

--- files/win32/Dialog.h
+++ files/win32/Dialog.h
@@ -19,7 +19,7 @@
 //---------------------------------------------------------------------------------------------
 // ** Function prototypes
 //---------------------------------------------------------------------------------------------
-HWND CreateDialogBox(HWND hWnd, HINSTANCE hInstance, const char *sTitle, int iWidth, int iHeight, DLGPROC DlgProc);
+HWND CreateDialogBox(HWND hWnd, HINSTANCE hInstance, const char *sTitle, int iWidth, int iHeight, DLGPROC DlgProc, int xPos = 0, int yPos = 0, bool bCenterWindow = true);
 
 
 //---------------------------------------------------------------------------------------------
