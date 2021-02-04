8f69f40c9e40e6af96ce983ad8342d4b2366a85e
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-04 11:44:35 +0100

    Update Controls.h

--- files/win32/controls/Controls.h
+++ files/win32/controls/Controls.h
@@ -17,9 +17,9 @@
 //---------------------------------------------------------------------------------------------
 // ** Controls  (using long long in case of 64-bit build, works in 32-bit as well)
 
-HWND CreateStaticTextCtrl(HWND hParentWnd, long long int ctrlID, const char szText[], int x, int y, int w = 100, int h = 30, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateStaticLineHoriz(HWND hParentWnd, long long int ctrlID, int x, int y, int w = 100, int h = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
-HWND CreateStaticLineVert(HWND hParentWnd, long long int ctrlID, int x, int y, int h = 100, int w = 5, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateStaticTextCtrl(HWND hParentWnd, const char szText[], int x, int y, int w = 100, int h = 30, long long int ctrlID = -1, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateStaticLineHoriz(HWND hParentWnd, int x, int y, int w = 100, int h = 5, long long int ctrlID = -1, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
+HWND CreateStaticLineVert(HWND hParentWnd, int x, int y, int h = 100, int w = 5, long long int ctrlID = -1, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
 HWND CreateButtonCtrl(HWND hParentWnd, long long int ctrlID, const char szText[], int x, int y, int w = 40, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL, void *fnCallback = NULL);
 HWND CreateEditCtrl(HWND hParentWnd, long long int ctrlID, const char szText[], int x, int y, int w = 50, int h = 14, DWORD dwStyle = NULL, DWORD dwExStyle = NULL, int iMaxLength = 0);
 HWND CreateComboBoxDropDownCtrl(HWND hParentWnd, long long int ctrlID, int x, int y, int w = 50, int h = 12, DWORD dwStyle = NULL, DWORD dwExStyle = NULL);
