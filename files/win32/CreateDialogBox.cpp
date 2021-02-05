50104881299d7f550756074641d50154883f4d6c
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-05 14:51:34 +0100

    Update CreateDialogBox.cpp

--- files/win32/CreateDialogBox.cpp
+++ files/win32/CreateDialogBox.cpp
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------------------------
 // ** CreateDialogBox.cpp
 //---------------------------------------------------------------------------------------------
-// Author: Keejelo
+// Author: keejelo
 // Github: https://github.com/keejelo/c_cpp_and_winapi_wrappers/tree/master/files/win32
 //---------------------------------------------------------------------------------------------
 // This creates a dialog template which is used as an empty skeleton dialog (without content).
