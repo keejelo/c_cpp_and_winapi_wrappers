ea37ce14a0cbb1d21bf390b490f2552a44b717ba
keejelo <34620895+keejelo@users.noreply.github.com>
2021-02-05 15:48:05 +0100

    Update CreateDialogBox.h

--- files/win32/CreateDialogBox.h
+++ files/win32/CreateDialogBox.h
@@ -20,7 +20,7 @@
 // ** Variables
 //---------------------------------------------------------------------------------------------
 
-// ** Struct to hold modal dialog info for passing it by REFERENCE as LPARAM ( (LPARAM)&modalinfo )
+// ** Struct to hold modal dialog info
 struct MODALINFO { const char *title; int x; int y; int w; int h; };
 
 
